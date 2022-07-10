4901aee69d25f6151b5c2c0b541dd3ab1b0736f7
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-10 19:02:13 -0300

    FEAT: Adicionando formulario para cadastrar o livro

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Aplicativo dos livros.</h1>
+      <form>
+        <h1>Livro</h1>
+        <label>Nome do livro:</label>
+        <input type="text" />
+        <label>Preço:</label>
+        <input type="text" />
+        <label>Nome do livro:</label>
+        <input type="text" />
+        <label for="genre">Escolha o gênero:</label>
+        <select name="genre">
+          <option value="">Selecione aqui</option>
+          <option value="Autoajuda">Autoajuda</option>
+          <option value="Ficção">Ficção</option>
+          <option value="Horror">Horror</option>
+          <option value="Humor">Humor</option>
+          <option value="Romance">Romance</option>
+        </select>
+      </form>
     </div>
   );
 }
-
-export default App;
