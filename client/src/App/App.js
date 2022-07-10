4ee8256cbfd655515571569f474d6a1f3267b184
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-10 19:53:16 -0300

    CHORE: Observando a mudanca dos valores inputados nos inputs

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,16 +3,24 @@ import React, { useState } from "react";
 
 export default function App() {
   const [formValues, setFormValues] = useState();
+
+  const handleChangeValues = (e) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div className="App">
       <form>
         <h1>Livro</h1>
-        <label for="name" >Nome do livro:</label>
-        <input type="text" name="name" required/>
-        <label for="price" >Preço:</label>
-        <input type="text" name="price" />
+        <label for="name">Nome do livro:</label>
+        <input type="text" name="name" onChange={handleChangeValues} required />
+        <label for="price">Preço:</label>
+        <input type="text" name="price" onChange={handleChangeValues} />
         <label for="genre">Escolha o gênero:</label>
-        <select name="genre" required>
+        <select name="genre" onChange={handleChangeValues} required>
           <option value="">Selecione aqui</option>
           <option value="Autoajuda">Autoajuda</option>
           <option value="Ficção">Ficção</option>
