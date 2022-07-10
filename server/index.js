55f6a98c130d8761c8b8afe0b9a9863854d79218
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-10 20:25:24 -0300

    TEST: Testando conexao com o banco de dados MySQL - sucesso

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
-const app = express()
-const PORT = 3001
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const PORT = 3001;
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "P@ssw0rd",
+  database: "library",
+});
 
 app.listen(PORT, () => {
-    console.log(`App rodando na porta ${PORT}`)
-})
+  console.log(`App rodando na porta ${PORT}`);
+});
+
+con.connect((err) => {
+  if (err) return console.log("Erro ao conectar ao banco de dados", err);
+  else console.log("Conexao estabelecida com sucesso ao banco de dados");
+});
+
+con.end((err) => {
+  if (err) return console.log("Erro ao finalizar o banco de dados", err);
+  console.log("A conexao com o banco de dados foi finalizada");
+});
